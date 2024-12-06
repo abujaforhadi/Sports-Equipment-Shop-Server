d75961b55de21dbeb58868496eda0bcd0e9f4f4a
abujaforhadi <abujaforhadi@gmail.com>
2024-12-07 01:12:20 +0600

    update

--- index.js
+++ index.js
@@ -66,13 +66,13 @@ const client = new MongoClient(uri, {
     });
     app.get("/data-limit", async (req, res) => {
       try {
-       
-        const data = await collection.find({}).limit(6).toArray();
+        const data = await collection.aggregate([{ $sample: { size: 6 } }]).toArray();
         res.json(data);
       } catch (error) {
-        res.status(500).send("Error fetching data from database");
+        res.status(500).send("Error fetching random data from database");
       }
     });
+    
     
 
     app.put("/data/:id", async (req, res) => {
